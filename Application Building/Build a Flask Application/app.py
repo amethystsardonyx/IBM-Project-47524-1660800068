f207939771b7ee661ea90e36ff57430c593c19ca
Rithick <113758658+Rithick16@users.noreply.github.com>
2022-11-18 11:37:38 +0530

    Add files via upload

---
+++ Application Building/Build a Flask Application/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, render_template, request
+
+app = Flask(_name_)
+
+@app.route('/', methods=['GET'])
+def index ():
+  #Home page
+  return render_template("template.html")
+
+  @app.route('/predict', methods =['GET','POST'])
+  def upload ():
+    # logic yet to be build 
+    if request.method == 'GET':
+      return ("Here the logic is defined")
+    if request.method == 'POST':
+      return ("Here the logic is defined")
+  if_name_ == '_main_':
+      app.run()
